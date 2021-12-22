23401d86299c77225d7424bf76c1e00a2924bcac
lucassouzamatos <lucas.ssouza.mmatos@gmail.com>
2021-12-22 10:56:53 -0300

    feat(init): ༼ つ ◕_◕ ༽つ

---
+++ git-inial-commit/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to git-inial-commit
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ git-inial-commit/test/git_inial_commit/core_test.clj
@@ -0,0 +1,7 @@
+(ns git-inial-commit.core-test
+  (:require [clojure.test :refer :all]
+            [git-inial-commit.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
