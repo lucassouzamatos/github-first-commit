1893932d60fab5027cd1eb5bd4d55c9901a0a8ef
lucassouzamatos <lucas.ssouza.mmatos@gmail.com>
2021-12-22 11:19:41 -0300

    "feat(testing): ༼ つ ◕_◕ ༽つ"

--- git-inial-commit/src/git_inial_commit/core.clj
+++ git-inial-commit/src/git_inial_commit/core.clj
@@ -1,7 +1,12 @@
 (ns git-inial-commit.core
+  (:require [clojure.java.shell :as shell])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (case (first args)
+    "random" (do
+               (println (:out (shell/sh "git" "add" ".")))
+               (println (:out (shell/sh "git" "commit" "-m" "\"feat(testing): ༼ つ ◕_◕ ༽つ\""))))
+    (println "select ")))
