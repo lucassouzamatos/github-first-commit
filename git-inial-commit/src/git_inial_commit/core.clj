da32880a2891d75127036e83b6fb6c37104520ce
lucassouzamatos <lucas.ssouza.mmatos@gmail.com>
2021-12-22 11:36:46 -0300

    "feat(testing): ༼ つ ◕_◕ ༽つ"

--- git-inial-commit/src/git_inial_commit/core.clj
+++ git-inial-commit/src/git_inial_commit/core.clj
@@ -6,7 +6,11 @@
   "I don't do a whole lot ... yet."
   [& args]
    (do
+     (println "adding all to repository")
      (println (:out (shell/sh "git" "add" ".")))
+     (println "commiting with selected message")
      (println (:out (shell/sh "git" "commit" "-m" "\"feat(testing): ༼ つ ◕_◕ ༽つ\""))))
    (case (first args)
-    "--push" (println (:out (shell/sh "git" "push")))))
+    "--push" (do 
+               (println "pushing to repository...")
+               (println (:out (shell/sh "git" "push"))))))
